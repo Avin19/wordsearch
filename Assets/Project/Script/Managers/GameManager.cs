d8f2e54de4457dbd659026a2fc84e8241d5c5689
Sketh <www.sketh007@gmail.com>
2026-05-09 12:29:53 +0530

    Grid Fill | Highlight placement test
    - Setting Highlight anchoring to top-left
    Scripts
    - GridManager
        [=] Grid fill logic
        [=] Using a single TMP text for grid fill
    - GameManager
        [=] Testing Highlight placement
        [=] Using RectTransformUtility for initial touch

--- Assets/Project/Script/Managers/GameManager.cs
+++ Assets/Project/Script/Managers/GameManager.cs
@@ -7,22 +7,37 @@ public class GameManager : MonoBehaviour
     [SerializeField] private RectTransform _highlightImg;
     private Vector2 _intialTouchPos;
     private bool _initalPosSet;
-    private float _resRatio;
+    private Vector2 _resRatio;
 
-    private const float CANVAS_HEIGHT = 1280;
+    // private const float CANVAS_HEIGHT = 1544;
+
+    //              TEST
+    [SerializeField] private RectTransform _mainCanvas;
+    private float _canvasWidthOffset;
+
+    private const int HIGHLIGHT_BASE_WIDTH = 42;
 
     void Start()
     {
-        _resRatio = CANVAS_HEIGHT / Screen.height;
+        _resRatio.x = _mainCanvas.sizeDelta.x / Screen.width;
+        _resRatio.y = _mainCanvas.sizeDelta.y / Screen.height;
+
+        // _resRatio.x = Screen.width / _mainCanvas.sizeDelta.x ;
+        // _resRatio.y = Screen.height /_mainCanvas.sizeDelta.y ;
+
+        _canvasWidthOffset = _mainCanvas.sizeDelta.x / 2;
+        Debug.Log($"Screen Resolution: {Screen.width} | {Screen.height} | {_canvasWidthOffset}");
 
         UnityEngine.InputSystem.EnhancedTouch.EnhancedTouchSupport.Enable();
     }
 
     void LateUpdate()
     {
+        TestTouch();
     }
 
     private Vector2 screenPos;          // DEBUG
+    // private Vector2 debugScreenPos, canvasPoint;
     private void TestTouch()
     {
         if (Touch.activeTouches.Count != 0)
@@ -37,14 +52,27 @@ public class GameManager : MonoBehaviour
                 // _screenPos.x -= (Screen.width / 2);
                 _intialTouchPos = screenPos;
                 screenPos.y -= (Screen.height / 2);
-                _highlightImg.anchoredPosition = screenPos * _resRatio;
+
+                // debugScreenPos = _highlightImg.anchoredPosition = screenPos;
+                // _highlightImg.anchoredPosition = screenPos * _resRatio;
+
+                Vector2 canvasPoint;
+                // Convert Screen Point to Canvas Local Space
+                RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                    _mainCanvas,
+                    screenPos,
+                    null, // Use null for Screen Space - Overlay
+                    out canvasPoint
+                );
+                canvasPoint.x += _canvasWidthOffset - (HIGHLIGHT_BASE_WIDTH / 2);
+                _highlightImg.anchoredPosition = canvasPoint;
             }
             else
             {
                 float diff = screenPos.x - _intialTouchPos.x;
 
                 Vector2 finalSize = _highlightImg.sizeDelta;
-                finalSize.x = diff * _resRatio;
+                finalSize.x = (diff * _resRatio.x) + (HIGHLIGHT_BASE_WIDTH / 2);
                 _highlightImg.sizeDelta = finalSize;
             }
         }
--- Assets/Project/Script/Managers/GridManager.cs
+++ Assets/Project/Script/Managers/GridManager.cs
@@ -1,4 +1,8 @@
+#define TEST_GRID
+
 using System;
+using System.Text;
+
 using UnityEngine;
 
 namespace WordSearch
@@ -6,9 +10,75 @@ namespace WordSearch
     public class GridManager : MonoBehaviour
     {
         [SerializeField] private RectTransform _highlightImg;
-        internal void ProcessTileClick(int x, int y, TileStatus currentStatus)
+        [SerializeField] private TMPro.TMP_Text _gridText;
+
+        private string[] _wordArr = new string[TOTAL_ROWS];
+
+        private StringBuilder _gridBuilder;
+
+        private const int TOTAL_ROWS = 10;
+
+        private const string INDENT_TAG = "<indent={0}>";
+        private const int INDENT_VAL_INCREMENT = 45;
+
+        //              HIGHLIGHT
+        private const int HIGHLIGHT_BASE_WIDTH = 42;
+
+        //              TEST
+        private readonly string[] _testGrid = new string[]
         {
-            throw new NotImplementedException();
+            "ELEPHANTZO",
+            "RQWRCABDYE",
+            "BUTTERFLYB",
+            "PANDAFGHJQ",
+            "OCEANSKLAH",
+            "AEOGFZEBRA",
+            "GIRAFFEASF",
+            "DOLPHINPQM",
+            "DOJKENLKEM",
+            "MOUNTAINJR",
+        };
+        // private readonly string INDENT_TAG = $"<{INDENT_LABEL}={INDENT_VAL_INCREMENT}>";
+
+        private void Start()
+        {
+            _gridBuilder = new StringBuilder();
+
+#if TEST_GRID
+            TestFillGrid();
+#endif
+        }
+
+        #region TEST
+        private void TestFillGrid()
+        {
+            for (int i = 0; i < TOTAL_ROWS; i++)
+                _wordArr[i] = _testGrid[i];
+
+            Debug.Log($"Filling Grid");
+            FillGrid();
+        }
+        #endregion TEST
+
+        private void FillGrid()
+        {
+            int row, col;
+            for (row = 0; row < TOTAL_ROWS; row++)
+            {
+                _gridBuilder.AppendFormat(INDENT_TAG, 0);
+                for (col = 0; col < TOTAL_ROWS - 1; col++)
+                {
+                    _gridBuilder.AppendFormat("{0}", _wordArr[row][col]);
+                    _gridBuilder.AppendFormat(INDENT_TAG, (col + 1) * INDENT_VAL_INCREMENT);
+                }
+                _gridBuilder.AppendFormat("{0}", _wordArr[row][col]);
+                _gridBuilder.Append('\n');
+            }
+            _gridText.text = _gridBuilder.ToString();
+        }
+
+        public void ProcessTileClick(int x, int y, TileStatus currentStatus)
+        {
         }
     }
 }
