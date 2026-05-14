3363c0c1386be2a6b27dced3a56336f07ee40871
Sketh <www.sketh007@gmail.com>
2026-05-14 18:14:04 +0530

    Drag Snapping
    Scripts
    - GameManager
        [=] Using define to separate blocks for easier debugging
        [=] Drag snapping | Both X and Y

--- Assets/Project/Script/Managers/GameManager.cs
+++ Assets/Project/Script/Managers/GameManager.cs
@@ -1,3 +1,7 @@
+// #define DEBUG_CELL_PLACEMENT
+#define DEBUG_DRAG
+// #define DEBUG_CELL_PLACEMENT_OLD
+
 using UnityEngine;
 
 using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
@@ -23,7 +27,7 @@ public class GameManager : MonoBehaviour
     private const float HIGHLIGHT_X_OFFSET = 10f, HIGHLIGHT_Y_OFFSET = 11.1f, HIGHLIGHT_Y_OFF_MULT = 0.7f;         // Additional Offset to cover the beginning letter correctly
     private const int START_GRID_INDEX = 11;
 
-    private const float CELL_X_OFFSET = 19.95f, CELL_Y_OFFSET = -19.86f;
+    private const float CELL_OFFSET_X = 19.95f, CELL_OFFSET_Y = -23.5f;
     private const float CELL_SIZE_X = 45.1f, CELL_SIZE_Y = 48.2f;
 
     // private static Vector2 CanvasSize => new Vector2(720, 1280);
@@ -51,19 +55,24 @@ public class GameManager : MonoBehaviour
         TestTouch2();
     }
 
+#if DEBUG_CELL_PLACEMENT_OLD
     private Vector2 screenPos;          // DEBUG
     private Vector2 debugScreenPos;
     private Vector2 debugScreenOffsetPos;
 
-    // [SerializeField] private float debugHighLightYOffset = 11.1f;
-    // [SerializeField] private float debugHiYOffMult = 0.7f;              //
-    // [SerializeField] private int startTextGrid = 11;
-    // [SerializeField] private float debugTouchYOffset = 11;
+    [SerializeField] private float debugHighLightYOffset = 11.1f;
+    [SerializeField] private float debugHiYOffMult = 0.7f;              //
+    [SerializeField] private int startTextGrid = 11;
+    [SerializeField] private float debugTouchYOffset = 11;
+#endif
+
     private void TestTouch()
     {
         if (Touch.activeTouches.Count != 0)
         {
-            // Vector2 screenPos;
+#if !DEBUG_CELL_PLACEMENT_OLD
+            Vector2 screenPos;
+#endif
             screenPos = Touch.activeTouches[0].screenPosition;
             if (!_initalPosSet)
             {
@@ -85,14 +94,16 @@ public class GameManager : MonoBehaviour
                     out _intitalCanvasPos
                 );
                 // canvasPoint.x += _canvasWidthOffset - (INDENT_VAL_INCREMENT / 2);
+#if DEBUG_CELL_PLACEMENT_OLD
                 debugScreenPos = _intitalCanvasPos;
+#endif
 
                 _intitalCanvasPos.x += _canvasWidthOffset;
                 // Snap to Grid Cells
                 _intitalCanvasPos.x = Mathf.Floor(_intitalCanvasPos.x / INDENT_VAL_INCREMENT) * INDENT_VAL_INCREMENT
                             - HIGHLIGHT_X_OFFSET;
 
-#if VERTICAL_HIGHLIGHT_DEBUG
+#if DEBUG_CELL_PLACEMENT_OLD
                 _intitalCanvasPos.y -= debugTouchYOffset;
 #else
                 // _intitalCanvasPos.y -= HIGHLIGHT_Y_OFFSET;
@@ -100,7 +111,7 @@ public class GameManager : MonoBehaviour
 
                 int yIndex = Mathf.CeilToInt(_intitalCanvasPos.y / HIGHLIGHT_Y_SIZE);             // Kill me
 
-#if VERTICAL_HIGHLIGHT_DEBUG
+#if DEBUG_CELL_PLACEMENT_OLD
                 _intitalCanvasPos.y = yIndex * HIGHLIGHT_Y_SIZE
                             - debugHighLightYOffset - (debugHiYOffMult * debugHiYOffMult * (startTextGrid + yIndex));          // Yeah this sucks
 #else
@@ -136,15 +147,26 @@ public class GameManager : MonoBehaviour
             _initalPosSet = false;
     }
 
+#if DEBUG_CELL_PLACEMENT
     [SerializeField] private Vector2 debugCellSize = new Vector2(45.1f, 48.2f);
     [SerializeField] private Vector2 debugCellOffset = new Vector2(19.95f, -23.5f);
-    private Vector2 debugCellSnapPos;
-    private Vector2Int debugCellIndex;
+    private Vector2 cellSnapPos;
+    private Vector2Int cellIndex;
+    private Vector2 debugScreenPos;
+    private Vector2 debugScreenOffsetPos;
+    // private Vector2 screenPos;
+#endif
+
+#if DEBUG_DRAG
+    private Vector2 dragDiff;
+    private Vector2 dragPos;
+#endif
+
     private void TestTouch2()
     {
         if (Touch.activeTouches.Count != 0)
         {
-            // Vector2 screenPos;
+            Vector2 screenPos;
             screenPos = Touch.activeTouches[0].screenPosition;
             if (!_initalPosSet)
             {
@@ -162,6 +184,9 @@ public class GameManager : MonoBehaviour
                     null, // Use null for Screen Space - Overlay
                     out _intitalCanvasPos
                 );
+
+                // Snap to Grid Cells
+#if DEBUG_CELL_PLACEMENT
                 debugScreenPos = _intitalCanvasPos;
 
                 debugScreenOffsetPos.x = debugScreenPos.x + (_gridContent.sizeDelta.x / 2) - (HIGHLIGHT_BASE_WIDTH / 2);
@@ -169,23 +194,50 @@ public class GameManager : MonoBehaviour
 
                 _intitalCanvasPos = debugScreenOffsetPos;
 
-                // Snap to Grid Cells
-                debugCellSnapPos = _intitalCanvasPos;
-                debugCellIndex.x = Mathf.FloorToInt(_intitalCanvasPos.x / debugCellSize.x);
-                debugCellSnapPos.x = debugCellIndex.x * debugCellSize.x + debugCellOffset.x;
+                cellIndex.x = Mathf.FloorToInt(_intitalCanvasPos.x / debugCellSize.x);
+                cellSnapPos.x = cellIndex.x * debugCellSize.x + debugCellOffset.x;
 
-                debugCellIndex.y = Mathf.FloorToInt((_intitalCanvasPos.y * -1) / debugCellSize.y);      // Already know that this will go from minus to plus
-                debugCellSnapPos.y = debugCellIndex.y * debugCellSize.y * -1 + debugCellOffset.y;
+                cellIndex.y = Mathf.FloorToInt((_intitalCanvasPos.y * -1) / debugCellSize.y);      // Already know that this will go from minus to plus
+                cellSnapPos.y = cellIndex.y * debugCellSize.y * -1 + debugCellOffset.y;
+#else
+                _intitalCanvasPos.x = _intitalCanvasPos.x + (_gridContent.sizeDelta.x / 2) - (HIGHLIGHT_BASE_WIDTH / 2);
+                _intitalCanvasPos.y = _intitalCanvasPos.y - (_gridContent.sizeDelta.y / 2) + (HIGHLIGHT_BASE_WIDTH / 2);
 
-                // int yIndex = Mathf.CeilToInt(_intitalCanvasPos.y / HIGHLIGHT_Y_SIZE);             // Kill me
+                Vector2 cellSnapPos = _intitalCanvasPos;
+                cellSnapPos.x = Mathf.FloorToInt(_intitalCanvasPos.x / CELL_SIZE_X)
+                                * CELL_SIZE_X + CELL_OFFSET_X;
 
-                // _intitalCanvasPos.y = yIndex * HIGHLIGHT_Y_SIZE
-                //             - HIGHLIGHT_Y_OFFSET - (HIGHLIGHT_Y_OFF_MULT * HIGHLIGHT_Y_OFF_MULT
-                //             * (START_GRID_INDEX + yIndex));          // Yeah this sucks
-
+                cellSnapPos.y = Mathf.FloorToInt((_intitalCanvasPos.y * -1) / CELL_SIZE_X)
+                                * (CELL_SIZE_Y * -1) + CELL_OFFSET_Y;
+#endif
                 // Debug.Log($"HighLight Y: {yIndex}");
 
-                _highlightImg.anchoredPosition = debugCellSnapPos;
+                _intitalCanvasPos = cellSnapPos;
+                _highlightImg.anchoredPosition = cellSnapPos;
+            }
+            else
+            {
+
+#if !DEBUG_DRAG
+                Vector2 dragDiff = screenPos - _intialTouchPos;
+                Vector2 dragPos;
+#else
+                dragDiff = screenPos - _intialTouchPos;
+#endif
+
+                dragPos = _intitalCanvasPos;
+                dragPos.x += (dragDiff.x * _resRatio.x);
+                dragPos.x = Mathf.Floor(dragPos.x / CELL_SIZE_X) * CELL_SIZE_X + CELL_OFFSET_X;
+
+                dragPos.y += (dragDiff.y * _resRatio.y);
+                dragPos.y = Mathf.Floor(dragPos.y / CELL_SIZE_Y) * CELL_SIZE_Y + (CELL_OFFSET_Y * -1);
+
+                _highlightImg.anchoredPosition = dragPos;
+                // Debug.Log($"Drag finalPos: {dragPos}");
+
+                // Vector2 finalSize = _highlightImg.sizeDelta;
+                // finalSize.x = (diff * _resRatio.x) + (HIGHLIGHT_BASE_WIDTH / 2);
+                // _highlightImg.sizeDelta = finalSize;
             }
         }
         else
