53cfc98ad778c04a69fbd166723555236bbc32c1
Sketh <www.sketh007@gmail.com>
2026-05-15 22:50:18 +0530

    Vertical highlight placement | Highlight size adjustment
    - Making highlight pivot to top-left for easier vertical alignment
    Scripts
    - GameManager
        [=] Vertical highlight alignment
            <~> Vert offset is HIGHLIGHT_BASE_SIZE
        [=] Clamping to minimum cell size
        [=] Keeping inital grid index and calculating highLightSize from it

--- Assets/Project/Script/Managers/GameManager.cs
+++ Assets/Project/Script/Managers/GameManager.cs
@@ -13,7 +13,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] private RectTransform _highlightImg;
     [SerializeField] private RectTransform _gridContent;
     private Vector2 _intialTouchPos, _intitalCanvasPos;
-    private Vector2Int _prevCellIndex;
+    private Vector2Int _initalCellIndex, _prevCellIndex;
     private bool _initalPosSet;
     private Vector2 _resRatio;
 
@@ -23,14 +23,16 @@ public class GameManager : MonoBehaviour
     [SerializeField] private RectTransform _mainCanvas;
     private float _canvasWidthOffset;
 
-    private const float HIGHLIGHT_BASE_SIZE = 45.1f;
+    // private const float HIGHLIGHT_BASE_SIZE = 45f;
     private const float HIGHLIGHT_Y_SIZE = 48.75f;
     private const float HIGHLIGHT_X_OFFSET = 10f, HIGHLIGHT_Y_OFFSET = 11.1f, HIGHLIGHT_Y_OFF_MULT = 0.7f;         // Additional Offset to cover the beginning letter correctly
     private const int START_GRID_INDEX = 11;
 
-    private const float CELL_OFFSET_X = 19.95f, CELL_OFFSET_Y = -22.5f;
-    private const float CELL_SIZE_X = 45.1f, CELL_SIZE_Y = 48.2f;
-    private const float CELL_GAP = 30.56f;
+    private const float CELL_OFFSET_X = 19.76f, CELL_OFFSET_Y = -21.47f;            // -21.47 for top-left | 216.03 for left-center
+    private const float CELL_SIZE_X = 45.1f;
+    private const float HIGHLIGHT_EX_OFFSET = 0.35f;
+
+    private const float VERTICAL_ALIGNMENT = -90f, VERT_ALIGN_QUARTERNION = 0.7071068f;
 
     // private static Vector2 CanvasSize => new Vector2(720, 1280);
     // private static Vector2 CellSize => new Vector2(10, 10);
@@ -44,7 +46,8 @@ public class GameManager : MonoBehaviour
         // _resRatio.y = Screen.height /_mainCanvas.sizeDelta.y ;
 
         _canvasWidthOffset = _mainCanvas.sizeDelta.x / 2;
-        Debug.Log($"Screen Resolution: {Screen.width} | {Screen.height} | {_canvasWidthOffset}");
+        Debug.Log($"Screen Resolution: {Screen.width} | {Screen.height} | _canvasWidthOffset: {_canvasWidthOffset}\n" +
+                    $" _mainCanvas Res: {_mainCanvas.sizeDelta}");
 
         UnityEngine.InputSystem.EnhancedTouch.EnhancedTouchSupport.Enable();
 
@@ -202,8 +205,8 @@ public class GameManager : MonoBehaviour
                 _prevCellIndex.y = Mathf.FloorToInt((_intitalCanvasPos.y * -1) / debugCellSize.y);      // Already know that this will go from minus to plus
                 cellSnapPos.y = _prevCellIndex.y * debugCellSize.y * -1 + debugCellOffset.y;
 #else
-                _intitalCanvasPos.x = _intitalCanvasPos.x + (_gridContent.sizeDelta.x / 2) - (HIGHLIGHT_BASE_SIZE / 2);
-                _intitalCanvasPos.y = _intitalCanvasPos.y - (_gridContent.sizeDelta.y / 2) + (HIGHLIGHT_BASE_SIZE / 2);
+                _intitalCanvasPos.x = _intitalCanvasPos.x + (_gridContent.sizeDelta.x / 2) - (CELL_SIZE_X / 2);
+                _intitalCanvasPos.y = _intitalCanvasPos.y - (_gridContent.sizeDelta.y / 2) + (CELL_SIZE_X / 2);
 
                 Vector2 cellSnapPos = _intitalCanvasPos;
                 _prevCellIndex.x = Mathf.FloorToInt(_intitalCanvasPos.x / CELL_SIZE_X);
@@ -212,14 +215,20 @@ public class GameManager : MonoBehaviour
                 _prevCellIndex.y = Mathf.FloorToInt((_intitalCanvasPos.y * -1) / CELL_SIZE_X);
                 cellSnapPos.y = _prevCellIndex.y * (CELL_SIZE_X * -1) + CELL_OFFSET_Y;
 #endif
-                // Debug.Log($"HighLight Y: {yIndex}");
+                // Debug.Log($"Initial | _prevCellIndex: {_prevCellIndex}");
 
                 _intitalCanvasPos = cellSnapPos;
-                _highlightImg.anchoredPosition = cellSnapPos;
+                _initalCellIndex = _prevCellIndex;
+                _highlightImg.anchoredPosition = _intitalCanvasPos;
+
+                // Reset from previous selection
+                _highlightImg.sizeDelta = new Vector2(CELL_SIZE_X, CELL_SIZE_X);
+                _highlightImg.localRotation = Quaternion.identity;
+                _highlightImg.anchoredPosition = _intitalCanvasPos;
             }
             else
             {
-                return;          //  TEST
+                // return;          //  TEST
 
 #if !DEBUG_DRAG
                 Vector2 dragDiff = screenPos - _intialTouchPos;
@@ -235,15 +244,47 @@ public class GameManager : MonoBehaviour
                 dragPos.x = cellIndex.x * CELL_SIZE_X + CELL_OFFSET_X;
 
                 dragPos.y += (dragDiff.y * _resRatio.y);
-                cellIndex.y = Mathf.FloorToInt(dragPos.y / CELL_SIZE_X);
-                dragPos.y = cellIndex.y * CELL_SIZE_X - CELL_OFFSET_Y;
+                // Offsetting range from [200:-200] to [0:-400] so as to start from the top | This is for LEFT-CENTER
+                // Offsetting range from [-21:-450] to [0:-471] so as to start from the top | This is for TOP-LEFT
+                dragPos.y -= CELL_OFFSET_Y;
+                cellIndex.y = Mathf.CeilToInt((dragPos.y * -1) / CELL_SIZE_X);
+                dragPos.y = cellIndex.y * (CELL_SIZE_X * -1) + CELL_OFFSET_Y;
+                // Debug.Log($"_prevCellIndex: {_prevCellIndex} | cellIndex: {cellIndex} | dragPos: {dragPos}");
 
-                if (_prevCellIndex.x == cellIndex.x && _prevCellIndex.y == cellIndex.y)
+                // Check if the cell index has changed
+                if (_prevCellIndex.x != cellIndex.x || _prevCellIndex.y != cellIndex.y)
                 {
-                    // _highlightImg.sizeDelta =
+                    // Check whether horizontal or vertical
+                    Vector2 highLightSize = _highlightImg.sizeDelta;
+
+                    // Player is dragging vertical
+                    if (_initalCellIndex.x == cellIndex.x)
+                    {
+                        // Debug.Log($"Vertical");
+                        highLightSize.x = ((cellIndex.y - _initalCellIndex.y) * CELL_SIZE_X) + HIGHLIGHT_EX_OFFSET * (cellIndex.y + 1);
+
+                        // Adjustment for vertical alignment
+                        _highlightImg.localRotation = new Quaternion(0, 0, -VERT_ALIGN_QUARTERNION, VERT_ALIGN_QUARTERNION);
+                        _highlightImg.anchoredPosition = new Vector2(_intitalCanvasPos.x + CELL_SIZE_X, _intitalCanvasPos.y);
+                    }
+                    // Player is dragging horizontal
+                    else
+                    {
+                        // Debug.Log($"Horizontal");
+                        highLightSize.x = ((cellIndex.x - _initalCellIndex.x) * CELL_SIZE_X) + HIGHLIGHT_EX_OFFSET * (cellIndex.x + 1);
+
+                        _highlightImg.localRotation = Quaternion.identity;
+                        _highlightImg.anchoredPosition = _intitalCanvasPos;
+                    }
+
+                    // highLightSize.y = (cellIndex.y * CELL_SIZE_X); //+ HIGHLIGHT_EX_OFFSET * (cellIndex.y + 1);
+
+                    highLightSize.x = Mathf.Max(highLightSize.x, CELL_SIZE_X);          // Clamp to minimum cell size
+                    _highlightImg.sizeDelta = highLightSize;
                     _prevCellIndex = cellIndex;
+
+                    // _highlightImg.anchoredPosition = dragPos;
                 }
-                _highlightImg.anchoredPosition = dragPos;
                 // Debug.Log($"Drag finalPos: {dragPos}");
 
                 // Vector2 finalSize = _highlightImg.sizeDelta;
